56f3e21b7c4150457b4505a950d2a0a6c6c56c40
SalmaBenamor <s.benamor@kunbus.com>
2021-12-13 15:28:44 +0100

    jingle bells

--- xmas.py
+++ xmas.py
@@ -18,27 +18,16 @@ import time
 
 
 
-rpi1 = revpimodio2.RevPiModIO(autorefresh=True)
-rpi2 = revpimodio2.RevPiModIO(autorefresh=True)
-rpi3 = revpimodio2.RevPiModIO(autorefresh=True)
-rpi4 = revpimodio2.RevPiModIO(autorefresh=True)
-rpi5 = revpimodio2.RevPiModIO(autorefresh=True)
-rpi6 = revpimodio2.RevPiModIO(autorefresh=True)
-rpi7 = revpimodio2.RevPiModIO(autorefresh=True)
-rpi8 = revpimodio2.RevPiModIO(autorefresh=True)
-rpi9 = revpimodio2.RevPiModIO(autorefresh=True)
-rpi10 = revpimodio2.RevPiModIO(autorefresh=True)
-rpi11 = revpimodio2.RevPiModIO(autorefresh=True)
-rpi12 = revpimodio2.RevPiModIO(autorefresh=True)
+rpi = revpimodio2.RevPiModIO(autorefresh=True)
 
 
-
-
-def play_1():
-    rpi1.io.O_1.value = 1
+#play 1/4 note
+def play_4(io):
+    exec("rpi.io.O_" + str(io) + ".value = 1")  
     time.sleep(0.3)
-    rpi1.io.O_1.value = 0
+    exec("rpi.io.O_" + str(io) + ".value = 0")  
     time.sleep(0.3)
+
 
 def pause_2():
     time.sleep(0.2)
@@ -47,89 +36,87 @@ def pause_4():
     time.sleep(0.4)
 
 #Jingle
-play_1()
-play_2()
-
+play_4(1)
+play_4(2)
 #Bells
-play_11()
-play_12()
-pause_11()
+play_4(4)
+play_4(12)
+pause_2()
 
 #Jingle
-play_3()
-play_10()
+play_4(3)
+play_4(10)
 #Bells
-play_4()
-play_9()
-pause_2(),
+play_4(4)
+play_4(9)
+pause_2()
 
 
 #Jin
-play_5()
-play_6()
+play_4(5)
+play_4(6)
 pause_2()
 #Gle
-play_7()
-play_8()
+play_4(7)
+play_4(8)
 pause_2()
 #All
-play_3()
-play_4()
+play_4(3)
+play_4(4)
 pause_2()
 #The
-play_9()
-play_10()
+play_4(9)
+play_4(10)
 pause_2()
 #Way
-play_1()
-play_2()
+play_4(1)
+play_4(2)
 pause_4()
 
 
 #Oh What Fun
-play_11()
-play_12()
+play_4(11)
+play_4(12)
 pause_2()
 
 
 #it  
-play_5()
-play_6()
+play_4(5)
+play_4(6)
 pause_2()
 #Is
-play_7()
-play_8()
+play_4(7)
+play_4(8)
 pause_4()
 #To
-play_3()
-play_4()
+play_4(3)
+play_4(4)
 pause_2()
 #Ride
-play_9()
-play_10()
+play_4(9)
+play_4(10)
 pause_2()
 #In
-play_1()
-play_2()
+play_4(1)
+play_4(2)
 pause_2()
 #A
-play_11()
-play_12()
+play_4(11)
+play_4(12)
 pause_2()
 #One
-play_11()
-play_12()
+play_4(11)
+play_4(12)
 pause_2()
 #Horse
-play_1()
-play_2()
-
+play_4(1)
+play_4(2)
 pause_2()
 #Open
-play_9()
-play_10()
+play_4(9)
+play_4(10)
 pause_2()
 #Sleigh
-play_3()
-play_4()
+play_4(3)
+play_4(4)
 pause_4()
